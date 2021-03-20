0e8357d49f3a369204e125b19452e51c3e97619c
thetai3110 <trthetai3110@gmail.com>
2021-03-20 19:03:43 +0700

    add redirect rule

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import './App.css';
 import { useAuth } from './contexts/auth_context';
 import AuthProvider from './contexts/auth_context';
@@ -8,15 +8,6 @@ import HeaderComponent from './components/header/header.component';
 import FooterComponent from './components/footer/footer.component';
 import React from 'react';
 import { Suspense } from 'react';
-// import LoginPage from './pages/login/login_page';
-// import ForgotPasswordPage from './pages/login/forgot_page';
-// import SignUpPage from './pages/login/signup_page';
-// import BlogPage from './pages/blog/blog_page';
-// import HomePage from './pages/home/home_page';
-// import DiscussPage from './pages/discuss/discuss_page';
-// import CodeOnlinePage from './pages/code_online_page/code_online_page';
-// import BlogDetailPage from './pages/blog/detail/blog_detail';
-// import BlogCreate from './pages/blog/create/blog_create';
 
 const BlogPage = React.lazy(() => import('./pages/blog/blog_page'));
 const HomePage = React.lazy(() => import('./pages/home/home_page'));
@@ -37,15 +28,7 @@ function App() {
         <div style={{ backgroundColor: '#f8f9fb' }}>
           <div style={{ paddingTop: '25px', paddingBottom: '25px' }} className="container-xl">
             <Suspense fallback={<div>Loading...</div>}>
-              <Switch>
-                <Route path="/" component={HomePage} exact></Route>
-                <Route path="/blog" component={BlogPage} exact></Route>
-                <Route path="/blog/:page" component={BlogPage} exact></Route>
-                <Route path="/blog/detail/:id" render={(props) => <BlogDetailPage {...props} />} exact></Route>
-                <Route path="/discuss" render={() => <DiscussPage />} exact></Route>
-                <Route path="/code-online" render={() => <CodeOnlinePage />} exact></Route>
-                <Route path="/create-blog" render={(props) => <BlogCreate {...props} />}></Route>
-              </Switch>
+              <AppRouter />
             </Suspense>
           </div>
         </div>
@@ -54,13 +37,30 @@ function App() {
     );
   else return (
     <AuthProvider>
-      <Switch>
-        <Route path="/login" component={LoginPage}></Route>
-        <Route path="/signup" component={SignUpPage}></Route>
-        <Route path="/logout" component={Logout}></Route>
-        <Route path="/forgot-password" component={ForgotPasswordPage}></Route>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/login" component={LoginPage}></Route>
+          <Route path="/signup" component={SignUpPage}></Route>
+          <Route path="/logout" component={Logout}></Route>
+          <Route path="/forgot-password" component={ForgotPasswordPage}></Route>
+        </Switch>
+      </Suspense>
     </AuthProvider>
+  )
+}
+
+const AppRouter = () => {
+  const { currentUser } = useAuth();
+  return (
+    <Switch>
+      <Route path="/" component={HomePage} exact></Route>
+      <Route path="/blog" component={BlogPage} exact></Route>
+      <Route path="/blog/:page" component={BlogPage} exact></Route>
+      <Route path="/blog/detail/:id" render={(props) => <BlogDetailPage {...props} />} exact></Route>
+      <Route path="/discuss" render={() => <DiscussPage />} exact></Route>
+      <Route path="/code-online" render={() => <CodeOnlinePage />} exact></Route>
+      <Route path="/create-blog" render={(props) => currentUser ? <BlogCreate {...props} /> : <Redirect to='/login' />}></Route>
+    </Switch>
   )
 }
 
